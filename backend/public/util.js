eb005426a8acb8c49d41c7be4c9e0c0dc0195f10
Louis <33809479+LouisPetrik@users.noreply.github.com>
2024-03-15 22:24:39 +0100

    redundante funktionen rausgenommen und bei an falschen stellen draggable und nicht-draggable marker jeweils behoben

---
+++ backend/public/util.js
@@ -0,0 +1,40 @@
+/**
+ * Entwernt einen Wegpunkt aus der Liste der Wegpunkte
+ * @param {*} index in der Liste der Wegpunkte
+ */
+function removeWaypoint(index) {
+  var waypoints = routingControl.getWaypoints()
+  if (waypoints.length > 2) {
+    // Verhindern, dass die letzten zwei Wegpunkte entfernt werden
+    waypoints.splice(index, 1)
+    routingControl.setWaypoints(waypoints)
+  }
+
+  toggleSpeichernButtonAktiviert()
+}
+
+/**
+ * Gibt die URL des OSRM-Services zurück, abhängig davon, ob die Anwendung lokal oder auf Domain läuft
+ * @returns {string} URL des OSRM-Services
+ * Caddy leitet alles auf /route an OSRM weiter. Lokal über port 80, sonst über 443 da https dann aktiv.
+ */
+function getServiceURL() {
+  if (window.location.hostname === 'localhost') {
+    return 'http://localhost/route/v1'
+  } else {
+    return 'https://stadttouren.online/route/v1'
+  }
+}
+
+/**
+ * Gibt die Koordinaten der Wegpunkte zurück, als Array von Objekten mit lat und lng
+ * @returns {Array} Array mit Koordinaten der Wegpunkte
+ */
+function getPunkte() {
+  var waypoints = routingControl.getWaypoints()
+  var punkte = []
+  for (var i = 0; i < waypoints.length; i++) {
+    punkte.push(waypoints[i].latLng)
+  }
+  return punkte
+}
